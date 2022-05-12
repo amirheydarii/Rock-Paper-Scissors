9b4dba132c5e75df5f97e6916e0ab214ffad5d3d
Amir Hossein Heydari <amir123fg5@gmail.com>
2022-05-12 15:07:02 +0430

    complete logic of game and show result in console.log

--- app.js
+++ app.js
@@ -1,72 +1,51 @@
-let item = ['ROCK' , 'PAPER' , 'SCISSORS']
+let player1Counter = 0;
+let player2Counter = 0;
 
-function computerPlay () {
-   let ramdom = item[Math.floor((Math.random() * item.length))]
-   return ramdom;
+let item = ["ROCK"];
+
+function getRandom() {
+  let ramdom = item[Math.floor(Math.random() * item.length)];
+  return ramdom;
 }
 
-
-function playerSelection(params) {
-    let playerSelection = prompt("Rock , Paper , Scissors ?") 
-    let up = playerSelection.toUpperCase()
-    return up;
+function playRound(player1Selection, player2Selection) {
+  if (
+    (player1Selection === "ROCK" && player2Selection === "SCISSORS") ||
+    (player1Selection === "SCISSORS" && player2Selection === "PAPER") ||
+    (player1Selection === "PAPER" && player2Selection === "ROCK")
+  ) {
+    player1Counter++;
+    console.log("player1 win");
+  }
+  if (
+    (player2Selection === "ROCK" && player1Selection === "SCISSORS") ||
+    (player2Selection === "SCISSORS" && player1Selection === "PAPER") ||
+    (player2Selection === "PAPER" && player1Selection === "ROCK")
+  ) {
+    player2Counter++;
+    console.log("player1 lose");
+  }
+  if (player1Selection === player2Selection) {
+    tie = "tie";
+    console.log("tie");
+  }
 }
 
+let result = document.querySelector(".section-p1");
+let situation = document.querySelector(".section-p2");
+let playerChoose = document.querySelector(".player-choose");
+let playerSituation = document.querySelector(".player-situation");
+let computerChoose = document.querySelector(".computer-choose");
+let computerSituation = document.querySelector(".computer-situation");
+let playerRock = document.querySelector(".btn-rock");
+let playerpaper = document.querySelector(".btn-paper");
+let playerScissors = document.querySelector(".btn-scissors");
 
+playerRock.addEventListener("click", () => handleClick("ROCK"));
+playerpaper.addEventListener("click", () => handleClick("PAPER"));
+playerScissors.addEventListener("click", () => handleClick("SCISSORS"));
 
-function playRound(playerSelection , computerPlay) {
-    if (playerSelection == "ROCK" && computerPlay == "ROCK") {
-        return "it's a tie! Rock ties with Rock"
-    } 
-    else if (playerSelection == "PAPER" && computerPlay == "PAPER"){
-        return "it's a tie! paper ties with paper"
-    }
-    else if (playerSelection == "SCISSORS" && computerPlay == "SCISSORS"){
-        return "it's a tie! scissors ties with scissors"
-    }
-    else if (playerSelection == "ROCK" && computerPlay == "PAPER") {
-        lose++
-        return "you lose! Paper beats Rock "
-    } 
-    else if (playerSelection == "PAPER" && computerPlay == "ROCK") {
-        win++
-        return "you won! Paper beats Rock"
-    }
-    else if (playerSelection == "SCISSORS" && computerPlay == "ROCK") {
-        lose++
-        return "you lose! Rock beats Scissors"
-    }  
-    else if (playerSelection == "ROCK" && computerPlay == "SCISSORS") {
-        win++
-        return "you won! Rock beats  Scissors"
-    }  
-    else if (playerSelection == "SCISSORS" && computerPlay == "PAPER") {
-        win++
-        return "you won! Scissors beats Paper"
-    }
-    else if (playerSelection == "PAPER" && computerPlay == "SCISSORS") {
-        lose++
-        return "you lose! Scissors beats Paper"
-    }
+function handleClick(player1Selection) {
+  let player2Selection = getRandom();
+  playRound(player1Selection, player2Selection);
 }
-
-let win = 0;
-let lose = 0;
-
-function game() {
-    for (let i = 1; i <= 5; i++) {
-    
-         (playRound(playerSelection() , computerPlay()));
-    };
-
-    if (win > lose) {
-        return "YOOOO you win"
-    } else if (win < lose) {
-        return "OOOOPs you lose"
-    } else {
-        return "tie"
-    }   
-}
-
-console.log(game());
-
